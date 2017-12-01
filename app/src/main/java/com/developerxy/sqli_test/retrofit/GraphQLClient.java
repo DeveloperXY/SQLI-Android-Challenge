e0d075e2a7040bfe746b75d45cb2f5d9857ccb26
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-12-01 11:21:14 +0000

    Setup Retrofit.

---
+++ app/src/main/java/com/developerxy/sqli_test/retrofit/GraphQLClient.java
@@ -0,0 +1,8 @@
+package com.developerxy.sqli_test.retrofit;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 01/12/2017.
+ */
+public interface GraphQLClient {
+    
+}
---
+++ app/src/main/java/com/developerxy/sqli_test/retrofit/ServiceGenerator.java
@@ -0,0 +1,31 @@
+package com.developerxy.sqli_test.retrofit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 01/12/2017.
+ */
+public class ServiceGenerator {
+    private static final String BASE_URL = "http://192.168.43.53:8080/";
+
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+    private static Retrofit retrofit = builder.build();
+    private static HttpLoggingInterceptor logging = new HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BASIC);
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    public static <S> S createService(Class<S> serviceClass) {
+        if (!httpClient.interceptors().contains(logging)) {
+            httpClient.addInterceptor(logging);
+            builder.client(httpClient.build());
+            retrofit = builder.build();
+        }
+
+        return retrofit.create(serviceClass);
+    }
+}
